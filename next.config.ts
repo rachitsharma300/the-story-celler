55fa38cb1da8ad26fa5263ffbab970b86d36e549
rachitsharma300 <rachitsharma300@gmail.com>
2026-06-27 22:43:58 +0530

    feat: initialize core project structure, MongoDB integration, and sample flipbook gallery

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    resolveAlias: {
+      canvas: "./lib/empty-mock.ts",
+    },
+  },
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
+  },
 };
 
 export default nextConfig;
