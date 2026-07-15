3fdabb9d40314cdf2706f0cae7f48cfde788103e
rachitsharma300 <rachitsharma300@gmail.com>
2026-07-15 22:21:13 +0530

    feat: update next.config.ts to include output configuration for export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
   turbopack: {
     resolveAlias: {
       canvas: "./lib/empty-mock.ts",
